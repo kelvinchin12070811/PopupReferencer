0f70fa11d541bd84231eb9116c27e5289e6d66ee
Kelvin Chin <chinkaiwen@yahoo.com>
2019-06-01 22:53:44 +0800

    Finished basic idea of application

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,11 +1,41 @@
+//===========================================================================================================
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0.If a copy of the MPL was not distributed with this
+// file, You can obtain one at http ://mozilla.org/MPL/2.0/.
+//===========================================================================================================
+#include <exception>
 #include <qapplication.h>
+#include <qfile.h>
+#include <qmessagebox.h>
 #include "window/MainWindow.hpp"
+
+void loadStyleSheet(QWidget& widget)
+{
+	QFile file{ "styles/master.css" };
+	file.open(QIODevice::ReadOnly);
+	if (!file.isOpen())
+		throw std::runtime_error{ "Unable to load style file" };
+
+	QString qss{ file.readAll() };
+	widget.setStyleSheet(qss);
+}
 
 int main(int argc, char** argv)
 {
-	QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-	QApplication a{ argc, argv };
-	window::MainWindow w;
-	w.show();
-	return a.exec();
+	try
+	{
+		QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+		QApplication a{ argc, argv };
+
+		window::MainWindow w;
+		loadStyleSheet(w);
+		w.show();
+
+		return a.exec();
+	}
+	catch (const std::exception & e)
+	{
+		QMessageBox::critical(nullptr, "Exception occured", e.what());
+		return -1;
+	}	
 }
--- src/window/MainWindow.cpp
+++ src/window/MainWindow.cpp
@@ -1,3 +1,10 @@
+//===========================================================================================================
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0.If a copy of the MPL was not distributed with this
+// file, You can obtain one at http ://mozilla.org/MPL/2.0/.
+//===========================================================================================================
+#include <qstandardpaths.h>
+#include <qfiledialog.h>
 #include "MainWindow.hpp"
 
 namespace window
@@ -13,5 +20,21 @@ namespace window
 		this->setMaximumSize(size);
 
 		this->setWindowTitle("Popup Referencer");
+		connectWidgets();
+	}
+	
+	void MainWindow::browseLocal()
+	{
+		QString file = QFileDialog::getOpenFileName(this, "Select reference image",
+			QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation),
+			"*.jpg *.png *.gif");
+		if (file.isEmpty()) return;
+		
+		ui->urlField->setText(file);
+	}
+	
+	void MainWindow::connectWidgets()
+	{
+		connect(ui->browseLocalBtn, &QPushButton::clicked, this, &MainWindow::browseLocal);
 	}
 }
